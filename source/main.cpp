b6e80a6de9826b3faae7ad5578d74b9416fa78e7
Potato <cljwtg@gmail.com>
2025-02-23 17:13:55 +0800

    Now prints some license information when run

--- source/main.cpp
+++ source/main.cpp
@@ -2,6 +2,14 @@
 
 int main() {
     
+    std::cout << "Qobloqc Client version 0.0.1" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "This software is licensed under the GNU GPLv3." << std::endl;
+    std::cout << "For more information, see the LICENSE.txt file found in the source code." << std::endl;
+    std::cout << "You can get a copy of the source code from the following URL:" << std::endl;
+    std::cout << "https://github.com/potatoTheProgrammer/Qobloqc-Client" << std::endl;
+    std::cout << "" << std::endl;
+    
     std::cout << "Hello World!" << std::endl;
     return 0;
     
