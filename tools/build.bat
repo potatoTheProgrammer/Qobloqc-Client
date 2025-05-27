2f0e7858f4d85cd08f2535d7fa5971b5a2eaf6f8
Potato <cljwtg@gmail.com>
2025-05-27 20:22:15 +0800

    Added a data folder with stuff in it

--- tools/build.bat
+++ tools/build.bat
@@ -9,7 +9,7 @@ pause > nul
 
 echo.
 echo Compiling...
-g++ -std=c++17 -o build/Qobloqc.exe -I headers source/main.cpp source/display.cpp misc/icon.res -static-libgcc -static-libstdc++
+g++ -std=c++17 -o build/Qobloqc.exe -I headers -I data/generated source/main.cpp source/display.cpp misc/icon.res -static-libgcc -static-libstdc++
 echo.
 
 echo Done! Press any key to close the builder.
